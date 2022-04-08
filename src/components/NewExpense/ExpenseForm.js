d4127b5226047ce2edb146d8ade4beb92e55d49d
unknown <boud.huseyin@gmail.com>
2022-04-08 16:02:38 +0200

    passing the data bottom-up

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import "./ExpenseForm.css";
 
-const ExpenseForm = () => {
+const ExpenseForm = (props) => {
   const [enteredTitle, setEnteredTitle] = useState("");
   const [enteredAmount, setEnteredAmonut] = useState("");
   const [enteredDate, setEnteredDate] = useState("");
@@ -23,7 +23,7 @@ const ExpenseForm = () => {
       amount: enteredAmount,
       date: new Date(enteredDate),
     };
-    console.log(expenseData);
+   props.onSaveExpenseDate(expenseData);
     setEnteredTitle('')
     setEnteredAmonut('')
     setEnteredDate('')
