168634a2bc9a42d0d2da09b1326c89148e61e160
unknown <boud.huseyin@gmail.com>
2022-04-09 15:17:50 +0200

    added some logic

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -13,6 +13,19 @@ const Expenses = (props) => {
   const filteredExpenses = props.items.filter(expense => {
     return expense.date.getFullYear().toString() === filterYear
   })
+
+  let expensesContent = <p className="text__no">No expense found.</p>
+  if (filteredExpenses.length > 0) {
+    expensesContent = filteredExpenses.map((expense) => (
+      <ExpenseItem
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      />
+    ));
+  }
+  
   return (
     <div>
       <Card className="expenses">
@@ -20,14 +33,7 @@ const Expenses = (props) => {
           selected={filterYear}
           onChangeFilter={filterChangeHandler}
         />
-        {filteredExpenses.map((expense) => (
-          <ExpenseItem
-            key={expense.id}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        ))}
+        {expensesContent}
       </Card>
     </div>
   );
