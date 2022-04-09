b239b1dfb4eff920ee44dc05172bc2500dd5eb1c
unknown <boud.huseyin@gmail.com>
2022-04-09 12:50:53 +0200

    Added filterChangeHandler

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -6,10 +6,15 @@ import ExpenseFilter from "./ExpenseFilter";
 
 
 const Expenses = (props) => {
+
+  const filterChangeHandler = (selectedYear) => {
+    console.log(selectedYear)
+    console.log('in expenses')
+  }
   return (
     <div>
       <Card className="expenses">
-        <ExpenseFilter />
+        <ExpenseFilter onChangeFilter={filterChangeHandler} />
         <ExpenseItem
           title={props.items[0].title}
           amount={props.items[0].amount}
